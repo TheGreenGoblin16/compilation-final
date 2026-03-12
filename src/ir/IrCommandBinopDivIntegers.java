7881e92c912b73fe81b9ed827c07693933d18d38
yon22s <yonatansagi2@gmail.com>
2026-03-12 16:07:19 +0200

    also div

--- src/ir/IrCommandBinopDivIntegers.java
+++ src/ir/IrCommandBinopDivIntegers.java
@@ -18,6 +18,8 @@ public class IrCommandBinopDivIntegers extends IrCommand
 	public Temp t1;
 	public Temp t2;
 	public Temp dst;
+	public int max = (1<<15)-1;
+	public int min = -(1<<15);
 	
 	public IrCommandBinopDivIntegers(Temp dst, Temp t1, Temp t2)
 	{
@@ -35,10 +37,22 @@ public class IrCommandBinopDivIntegers extends IrCommand
 
 	public void mipsMe(){
 		String label = IrCommand.getFreshLabel("end");
+		String label1 = IrCommand.getFreshLabel("end");
+		String label2 = IrCommand.getFreshLabel("end");
 		MipsGenerator.getInstance().bnez(t2, label);
 		MipsGenerator.getInstance().printString("string_illegal_div_by_0");
 		MipsGenerator.getInstance().ExitAsm();
 		MipsGenerator.getInstance().label(label);
 		MipsGenerator.getInstance().div(dst,t1,t2);
+		
+		MipsGenerator.getInstance().li("$s0",min); // unnecessary but why not
+		MipsGenerator.getInstance().bge(dst,"$s0",label1);
+		MipsGenerator.getInstance().li(dst,min);
+		MipsGenerator.getInstance().jump(label2);
+		MipsGenerator.getInstance().label(label1);
+		MipsGenerator.getInstance().li("$s0",max); // from here necessary
+		MipsGenerator.getInstance().ble(dst,"$s0",label2);
+		MipsGenerator.getInstance().li(dst,max);
+		MipsGenerator.getInstance().label(label2);
 	}
 }
